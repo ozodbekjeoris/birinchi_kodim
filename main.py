9608608d050b6bc9022c1c4a34a4c61f43eb37f9
ozodbekjeoris <ozodbekroziqov74@gmail.com>
2024-11-27 09:22:05 +0500

    dictionary_haqida
    dectionary_haqida

---
+++ main.py
@@ -0,0 +1,70 @@
+# alist = [5, 6, 8, 9, "Ahror", "asasa"]
+# my_dict = {"name": "Ahror", "age": 30, "year": 1994}
+
+# for i in alist:
+#     print(i)
+
+
+# for i in range(50, 100, 3):
+#     print(i)
+
+
+# for i in my_dict:
+#     print(my_dict.get(i))
+
+
+# for k, v in enumerate(alist):
+#     print(k, v)
+
+
+# for i in range(100):
+#     if i==6:
+#         i=0
+#     print(i)
+
+# fruct_list = ['olma', 'gilos', 'ananas', 'anor']
+#
+# k = 0
+# n=int(input("n ni kiriting: "))
+# for i in range(n+1):
+#     print(i**2)
+# print(k)
+
+# alist=['olma', 'banan', 'uzum','ananas', 'mandarin']
+# blist=['olma', 'gilos', 'uzum', 'mandarin']
+# empty=[]
+#
+# for i in alist:
+#     for k in blist:
+#         if i==k:
+#             empty.append(i)
+#
+# print(empty)
+# num=1
+#
+# while num<=5:
+#     print(num)
+#     num+=1
+
+# n = int(input("break qiladigan sonni kiriitng: "))
+
+# for i in range(10):
+#     for k in range(5):
+#         print(i)
+#         if k == n:
+#             break
+
+# for i in range(20):
+#     if i == n:
+#         continue
+#
+#     print(i)
+
+# t = int(input("birinchi sonni kiriitng: "))
+# while True:
+#     son = int(input("sonni kiriitng: "))
+#     if son == 0:
+#         break
+#     if son > t:
+#         t = son
+# print(t)
